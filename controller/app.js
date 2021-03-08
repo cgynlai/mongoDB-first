47d1e08a8d24add044eb560632c839c8d083e41c
ylchong <cgynlai@gmail.com>
2021-03-08 20:54:06 +0800

    post request implemented

--- controller/app.js
+++ controller/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const Blog = require('../models/blog')
 
 const app = express();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(urlencodedParser);
+app.use(bodyParser.json());
 
 
 const dbURI = 'mongodb+srv://regularUser:regularUser123@mongo1.fvf5m.mongodb.net/firstDB?retryWrites=true&w=majority';
@@ -34,4 +38,24 @@ app.get('/single-blog', (req, res) => {
     Blog.findById('6043931012fd6d0ff411212b')
     .then(result => res.send(result))
     .catch(err=>console.log(err))
+})
+
+app.post('/add-use-post', (req,res) => {
+    var newTitle = req.body.title;
+    var newSnippet = req.body.snippet;
+    var newBody = req.body.body;
+
+    const blog = new Blog({
+        title : newTitle,
+        snippet : newSnippet,
+        body : newBody
+    })
+
+    blog.save( function(err,result){
+        if(err){
+            console.log(err)
+        }else {
+            res.send(result);
+        }
+    })
 })
